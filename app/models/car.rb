870cbc846d072b68e2317df2462f4cc3a2aabaea
itayariel <itayrel@gmail.com>
2018-04-14 21:41:15 +0300

    make title required field

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,5 @@ class Car < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates :image, attachment_presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates :title, :presence => true
 end
